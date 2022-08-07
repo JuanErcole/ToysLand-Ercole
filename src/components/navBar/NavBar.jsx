e5f0ebe3352fdb4d06310aa85b17f051eef2d3c8
Juan Ercole <juan.m.ercole@gmail.com>
2022-08-06 22:06:51 -0300

    creando el navbar

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './NavBar.css' 
+import carrito from '../../carrito.svg'
+
+export default function NavBar() {
+  return (
+    <div className='header'> 
+      <h1>ToysLand</h1>
+      <div className="links">
+        <ul>
+          <li><a href="">Inicio</a></li>
+          <li><a href="">Categorias</a></li>
+          <li><a href="">Nosotros</a></li>
+        </ul>
+        <img src={carrito} className='imgCarrito' alt="imagen carrito" />
+      </div>
+
+
+    </div>
+  )
+}
