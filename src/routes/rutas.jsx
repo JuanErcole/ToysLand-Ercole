a40b463e2fcff280e1db7372509b0f8daae99c5c
Juan Ercole <juan.m.ercole@gmail.com>
2022-10-17 11:06:12 -0300

    agregando productos

--- src/routes/rutas.jsx
+++ src/routes/rutas.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom' /*imrb */
 import { HeroContainer } from '../components/HeroContainer/HeroContainer';
 import ItemDetailContainer from '../components/ItemDetailContainer/ItemDetailContainer';
-import ItemDetail from '../components/ItemDetail/ItemDetail';
 import NavBar from '../components/navBar/NavBar';
 import ItemListContainer from '../components/ItemListContainer/ItemListContainer';
 import { Cart } from '../components/cart/Cart';
